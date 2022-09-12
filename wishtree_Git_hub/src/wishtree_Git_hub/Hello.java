c90212ef2ad58a95e219cd06d06757febd77655e
comp <comp@DESKTOP-2BG9DUM>
2022-09-12 17:22:29 +0530

    work commited

---
+++ wishtree_Git_hub/src/wishtree_Git_hub/Hello.java
@@ -0,0 +1,10 @@
+package wishtree_Git_hub;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello");
+	}
+
+}
---
+++ wishtree_Git_hub/src/wishtree_Git_hub/second.java
@@ -0,0 +1,12 @@
+package wishtree_Git_hub;
+
+public class second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int a=20;
+      int b=20;
+      System.out.println(a+b);
+	}
+
+}
